fbaadc3ae14eef0d3589526dcda4fba2907bdaa5
fnhnafiz <nafsinsifan2k21@gmail.com>
2025-06-28 14:46:38 +0600

    join venture ai job task instalation first time and start my task

---
+++ README.md
@@ -0,0 +1,3 @@
+# Join Venture AI JOB TASK
+
+
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className='font-bold text-red-600 text-5xl'>Welcome to Join Venture AI</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
